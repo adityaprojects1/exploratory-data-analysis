3a16c6cc84d446f5f181f1342502f975579cd3cd
adityaprojects1 <bhardwajaditya325@gmail.com>
2024-12-22 00:36:35 +0530

    Add files via upload

---
+++ eda.sql
@@ -0,0 +1,94 @@
+# exploratoray data analysis
+
+select*
+from layoffs_staging2;
+
+SELECT MAX(total_laid_off),max(percentage_laid_off)
+from layoffs_staging2;
+
+
+select*
+from layoffs_staging2
+where percentage_laid_off=1
+order by funds_raised_millions desc;
+
+select company, sum(total_laid_off)
+from layoffs_staging2
+group by company
+order by 2 desc;
+
+
+select min(`date`), max(`date`)
+from layoffs_staging2;
+
+
+select industry, sum(total_laid_off)
+from layoffs_staging2
+group by industry
+order by 2 desc;
+
+
+select country, sum(total_laid_off)
+from layoffs_staging2
+group by country
+order by 2 desc;
+
+select year(`date`), sum(total_laid_off)
+from layoffs_staging2
+group by year(`date`)
+order by 1 desc;
+
+
+select stage, sum(total_laid_off)
+from layoffs_staging2
+group by stage
+order by 2 desc;
+
+
+select company, avg(percentage_laid_off)
+from layoffs_staging2
+group by company
+order by 2 desc;
+
+
+select substring(`date`,1,7) as `month`,sum(total_laid_off)
+from layoffs_staging2
+where substring(`date`,1,7) is not null
+group by `month`
+order by 1 asc ;
+
+
+
+with rolling_total as 
+
+(select substring(`date`,1,7) as `month`,sum(total_laid_off) as total_off
+from layoffs_staging2
+where substring(`date`,1,7) is not null
+group by `month`
+order by 1 asc
+)
+select  `month`, total_off,
+sum(total_off)over (order by  `month`) as rolling_total
+from rolling_total;
+
+
+WITH Company_Year AS 
+(
+  SELECT company, YEAR(date) AS years, SUM(total_laid_off) AS total_laid_off
+  FROM layoffs_staging2
+  GROUP BY company, YEAR(date)
+)
+, Company_Year_Rank AS (
+  SELECT company, years, total_laid_off, DENSE_RANK() OVER (PARTITION BY years ORDER BY total_laid_off DESC) AS ranking
+  FROM Company_Year
+)
+SELECT company, years, total_laid_off, ranking
+FROM Company_Year_Rank
+WHERE ranking <= 3
+AND years IS NOT NULL
+ORDER BY years ASC, total_laid_off DESC;
+
+
+	 
+
+
